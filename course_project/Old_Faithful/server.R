ba0587aa56c19fac49533068b93116cef6782479
downtownadam <the.adam.blair@gmail.com>
2016-03-28 22:25:54 -0500

    Created Web App

---
+++ course_project/Old_Faithful/server.R
@@ -0,0 +1,37 @@
+library(shiny)
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+   
+  output$xyPlot <- renderPlot({
+    # generate bins based on input$bins from ui.R
+    condDist <- faithful[faithful$waiting > input$waited,]
+    z <- lm(eruptions~waiting,data=condDist)
+    #y < - faithful[faithful$waiting > input$waited,"eruptions"]
+    titleText <- c(paste("Expected Time Until Eruption: ",
+                         round(mean(condDist$waiting)-input$waited,1)," minutes"),
+                   paste("Expected Duration of Eruption: ",
+                         round(mean(condDist$eruptions),1)," minutes"),
+                   paste("Likelihood of Eruption within Next Five Minutes: ",
+                         round(mean(condDist$waiting <= input$waited+5)*100,1),"%")
+                   
+                  )
+                   
+    # draw the histogram with the specified number of bins
+    plot(y=faithful$eruptions,x=faithful$waiting,
+         xlab="Waiting Time",
+         ylab="Eruption Length (mins)",
+         main=titleText)
+    abline(z,wt=2,col="blue")
+    abline(v=input$waited)
+    points(x=mean(faithful[faithful$waiting>input$waited,"waiting"]),
+           y=mean(faithful[faithful$waiting>input$waited,"eruptions"]),
+           type="p", col="red",
+           pch=19, cex=2)
+    
+    
+    
+    
+  })
+  
+})
---
+++ course_project/Old_Faithful/ui.R
@@ -0,0 +1,33 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+  
+  # Application title
+  titlePanel("Old Faithful Geyser Data"),
+  
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(
+       sliderInput("waited",
+                   "How long have you been waiting:",
+                   min = 0,
+                   max = 92,
+                   value = 0)
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+       plotOutput("xyPlot")
+    )
+  )
+))
